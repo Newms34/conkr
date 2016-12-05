2d0559a7a8d08224dec283d03729d2e9ca109eee
newms34 <newms3450@gmail.com>
2016-12-05 18:24:18 -0500

    chat works! now you can talk with people and insult them while you play.

--- build/js/controllers/chatCon.js
+++ build/js/controllers/chatCon.js
@@ -1,21 +1,64 @@
-var app = angular.module('conkr', []).controller('chatController',function($scope,mapFact){
-	$scope.msgs = [];
-	$scope.user = null;
-	$scope.msgInp = '';
-	miscFact.getUsr().then(function(r){
-		if(!r.data||r.data=='none'){
-			throw new Error('User not found or not logged in!')
-		}else{
-			$scope.user = r.data;
-		}
-	})
-	socket.on('newMsg',function(msg){
-		if($scope.msgs.length>10){
-			$scope.msgs.shift();
-		}
-		$scope.msgs.push(msg)
-	})
-	$scope.sendMsg = function(){
-		socket.emit('sendMsg',{msg:$scope.msgInp,usr:$scope.user});
-	}
+var app = angular.module('conkr', []).controller('chatController', function($scope, mapFact, miscFact) {
+	$scope.prevSent = [];
+    $scope.msgs = [{
+    	now:new Date().toLocaleTimeString(),
+    	usr:'system',
+    	msg:'Welcome to Conkr! chat. Type /inv to switch color modes, or /time to toggle timestamp.'
+    }];
+    $scope.user = null;
+    $scope.msgInp = '';
+    miscFact.getUsr().then(function(r) {
+        if (!r.data || !r.data.name) {
+            throw new Error('User not found or not logged in!')
+        } else {
+            $scope.user = r.data.name;
+        }
+    })
+    socket.on('newMsg', function(msg) {
+        msg.now = new Date().toLocaleTimeString();
+        // if ($scope.msgs.length>1 && !$scope.maxMsg)
+        if ($scope.msgs.length > 8) {
+            $scope.msgs.shift();
+        }
+        $scope.msgs.push(msg);
+        console.log($scope.msgs)
+        $scope.$digest();
+        $('.chat-cont').scrollTop(parseInt($('.chat-cont').height()))
+    })
+    $scope.sendMsg = function() {
+        if ($scope.msgInp == '/time') {
+            $scope.timeStamp = !$scope.timeStamp;
+        } else if ($scope.msgInp == '/inv') {
+        	$scope.invCol = !$scope.invCol;
+        } else {
+            socket.emit('sendMsg', { msg: $scope.msgInp, usr: $scope.user });
+        }
+        $scope.prevSent.push($scope.msgInp);
+        $scope.currPrevMsg = $scope.prevSent.length;
+        $scope.msgInp = '';
+        $('#msgInp').focus();
+    }
+    document.querySelector('#msgInp').addEventListener('keydown',function(e){
+    	//38 == up, 40==down
+    	if (e.which==38){
+    		e.preventDefault();
+    		if($scope.prevSent.length && $scope.currPrevMsg>0){
+    			$scope.currPrevMsg--;
+    			$scope.msgInp = $scope.prevSent[$scope.currPrevMsg];
+    		}
+    		$scope.$digest();
+    	}else if(e.which==40){
+    		e.preventDefault();
+    		if($scope.prevSent.length && $scope.currPrevMsg<$scope.prevSent.length-1){
+    			$scope.currPrevMsg++;
+    			$scope.msgInp = $scope.prevSent[$scope.currPrevMsg];
+    		}
+    		$scope.$digest();
+    	}else if(e.which==27){
+    		e.preventDefault();
+    		$scope.currPrevMsg = $scope.prevSent.length;
+    		$scope.msgInp = '';
+    		$scope.$digest();
+    	}
+    })
 })
